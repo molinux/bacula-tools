d57fddfd9b754ef1c3c6f317e95085353cc72bd2
Marcus Molinero <cybercrowbr@gmail.com>
2025-07-17 01:08:19 -0300

    Update check-volretention.sh
    Listagem de volumes separadas por pool

--- bacula-retention-time/check-volretention.sh
+++ bacula-retention-time/check-volretention.sh
@@ -4,14 +4,17 @@
 # Descrição:      Script para verificação do tempo de expiração de todos os volumes
 # Autor:          Marcus Molinero
 # Contato:        marcusmolinero@nomalad.com.br
-# Versão:         0.0.1
+# Versão:         0.0.2
 # Data:           17/07/2025
-# Atualização:    17/07/2025 - Adicionado suporte para compressão gzip
+# Atualização:    17/07/2025 - Listagem de volumes separadas por pool
 # Licença:        GNU GPLv3
 
 for POOL in $(echo list pools | bconsole | awk 'NR >= 10 && NR < 24 { print $4}')
 do
-  for VOLUME in in $(echo list media pool=$POOL | bconsole | grep FULL | awk '{print $4}')
+  echo ===== POOL $POOL =====
+  echo llist pool=$POOL | bconsole | grep -e name -e volretention
+  echo
+  for VOLUME in $(echo list media pool=$POOL | bconsole | grep FULL | awk '{print $4}')
   do
     echo llist volume=$VOLUME | bconsole | grep -e volumename -e volretention
   done
